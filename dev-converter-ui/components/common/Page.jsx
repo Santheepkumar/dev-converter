fdb9c4d0483685ddc7ccd4bda00febee65d4faf6
Sanjaikumar <sanjaikumarbca@gmail.com>
2021-07-28 13:23:43 +0530

    Page

--- dev-converter-ui/components/common/Page.jsx
+++ dev-converter-ui/components/common/Page.jsx
@@ -3,50 +3,63 @@ import React from "react";
 function Page() {
   return (
     <div className=' max-w-700  bg-dark-900 border rounded-2xl border-dark-900 '>
-      <div className=' mt-5 text-3xl text-dark-100 text-center'>
-        <h1>Dev converter</h1>
+      <div className=' mt-7 text-3xl text-dark-100 text-center'>
+        <h1>Converter collections</h1>
       </div>
-      <h1 className='ml-14 text-2xl text-indigo-50 mt-5'>
+      <h1 className='ml-20 text-2xl text-indigo-50 mt-5'>
         Collection of methods:
       </h1>
-      <div className=' items-center max-w-2xl ml-40'>
-        <div className=' ml-14  mt-5 p-3 border rounded-xl hover:bg-dark-800 border-gray-400  '>
-          <h1 className=' text-white text-2xl hover:underline'>
-            1. String method
-          </h1>
-          <h2 className='mt-2 mb-2 text-dark-400'>snippet collection</h2>
-          <p className='text-dark-200  hover:underline'>
-            Select the string method to apply you needed function
-          </p>
-        </div>
+      <div className=' flex items-center ml-'>
+        <div className='ml-56 mt-9 mb-12'>
+          <div
+            className=' flex mb-10 hover:bg-gray-700 border rounded-xl  border-gray-600 
+               '>
+            <h1 className=' px-4 py-3 text-white bg rounded-xl  text-2xl bg-yellow-500 '>
+              ST
+            </h1>
+            <h1 class=' p-2 mt-2 text-indigo-50 '>String method</h1>
+          </div>
 
-        <div className=' ml-14 mt-7 p-3 border rounded-xl hover:bg-dark-800 border-gray-400 '>
-          <h1 className='text-white text-2xl  hover:underline'>
-            {" "}
-            2. Object method
-          </h1>
-          <h2 className='text-dark-400 mt-2 mb-2'>snippet collection</h2>
-          <p className='text-dark-200  hover:underline'>
-            Select the object method to apply you needed function
-          </p>
+          <div className='mt-5 flex border rounded-xl hover:bg-gray-700 border-gray-600   '>
+            <h1 className='p-3 bg-blue-600 bg rounded-xl text-white  text-2xl '>
+              OB
+            </h1>
+            <h1 className='text-indigo-50 mt-2 p-2'> Object method</h1>
+          </div>
         </div>
-        <div className=' ml-14 mt-7 p-3 border rounded-xl hover:bg-dark-800 border-gray-400 '>
-          <h1 className='text-white text-2xl  hover:underline'>
-            3. Dom method
-          </h1>
-          <h2 className='text-dark-400 mt-2 mb-2'>snippet collection</h2>
-          <p className='text-dark-200  hover:underline'>
-            Select the dom method to apply you needed function
-          </p>
+        <div className='ml-9 mb-4 mt-1 '>
+          <div
+            className='  flex mb-10 hover:bg-gray-700 border rounded-xl  border-gray-600 
+               '>
+            <h1 className='px-4 py-3 text-indigo-100 bg rounded-xl  text-2xl bg-red-500 '>
+              ND
+            </h1>
+            <h1 class='mt-2 p-2 text-indigo-50 '>Node method</h1>
+          </div>
+
+          <div className=' flex  border rounded-xl hover:bg-gray-700 border-gray-600   '>
+            <h1 className='px-4 py-3 bg-pink-600 bg rounded-xl text-white  text-2xl '>
+              MT
+            </h1>
+            <h1 className=' text-indigo-50 mt-2 p-2'> Math method</h1>
+          </div>
         </div>
-        <div className='ml-14 mb-10 mt-7 p-3 border rounded-xl hover:bg-dark-800 border-gray-400 '>
-          <h1 className=' text-white text-2xl  hover:underline'>
-            4. Array method
-          </h1>
-          <h2 className='text-dark-400 mt-2 mb-2'>snippet collection</h2>
-          <p className='text-dark-200  hover:underline'>
-            Select the array method to apply you needed function
-          </p>
+        <div className='ml-9 mb-3'>
+          <div
+            className=' flex mb-10 hover:bg-gray-700 border rounded-xl  border-gray-600 
+               '>
+            <h1 className='px-4 py-3 text-indigo-100 bg rounded-xl  text-2xl bg-purple-700 '>
+              FN
+            </h1>
+            <h1 class='mt-2 p-2 text-indigo-50 '>Function method</h1>
+          </div>
+
+          <div className='mt-3 flex  border rounded-xl hover:bg-gray-700 border-gray-600   '>
+            <h1 className='p-3 bg-green-700 bg rounded-xl text-white  text-2xl '>
+              DM
+            </h1>
+            <h1 className='mt-2 px-5 text-indigo-50 p-2'> Dom method</h1>
+          </div>
         </div>
       </div>
     </div>
