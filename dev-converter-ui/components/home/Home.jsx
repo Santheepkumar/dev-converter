6e952955fcf878b3e726726841f3a680bc9fdaee
Sanjaikumar <sanjaikumarbca@gmail.com>
2021-07-31 14:55:24 +0530

    Home.jsx

--- dev-converter-ui/components/home/Home.jsx
+++ dev-converter-ui/components/home/Home.jsx
@@ -4,24 +4,31 @@ import { getColorByKey } from "../../utils/color.util"
 function Home({ collections }) {
   return (
     <>
-      <div className="mt-7 text-3xl text-dark-100 text-center">
+      <div className="font-serif font-bold mt-7 text-4xl text-purple-500 text-center">
         <h1>Converter collections</h1>
       </div>
       <div className="max-w-7xl mx-auto">
         <div className="flex flex-wrap justify-evenly mt-10 border rounded-2xl border-gray-400 shadow p-8 gap-y-8">
-          {collections.map(({ short, label, path }, key) => (
+          {collections.map(({ short, label, path, extra }, key) => (
             <Link href={path}>
-              <a className="w-1/3 flex justify-center">
-                <div className="flex hover:bg-gray-700 border rounded-xl border-gray-600 w-72">
-                  <h1
-                    className="p-5 text-white bg rounded-xl  text-4xl"
-                    style={{
-                      backgroundColor: getColorByKey(key)
-                    }}
-                  >
-                    {short}
-                  </h1>
-                  <h1 className="mt-7 ml-6   text-gray-50 ">{label}</h1>
+              <a className="w-1/3 ">
+                <div className="flex bg-gray-900 hover:bg-gray-600 border rounded-xl border-gray-600 w-80">
+                  <div>
+                    <h1
+                      className=" px-5 py-6 text-white bg rounded-xl  text-3xl"
+                      style={{
+                        backgroundColor: getColorByKey(key)
+                      }}
+                    >
+                      {short}
+                    </h1>
+                  </div>
+                  <div>
+                    <h1 className="ml-3 mt-4 text-white font-bold text-lg ">
+                      {label}
+                    </h1>
+                    <h1 className="text-gray-300 ml-3">{extra}</h1>
+                  </div>
                 </div>
               </a>
             </Link>
