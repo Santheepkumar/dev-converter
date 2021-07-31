b62e8aa85e47c5e3e11a6c32e9a6f39b2b56aa0a
Santheep kumar <santheep.v@skematix.in>
2021-07-31 13:29:45 +0530

    some fix

--- dev-converter-ui/next.config.js
+++ dev-converter-ui/next.config.js
@@ -4,11 +4,6 @@ module.exports = {
     // Warning: Dangerously allow production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-    eslint: {
-      // Warning: Dangerously allow production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true
-    }
   },
   async redirects() {
     return [
