2e86e8078b543f6fb0882ab9d3364080c9484cd5
Santheep kumar <santheep.v@skematix.in>
2021-08-01 01:29:05 +0530

    Home screen responsive fixes

--- dev-converter-ui/components/home/Home.jsx
+++ dev-converter-ui/components/home/Home.jsx
@@ -4,14 +4,14 @@ import { getColorByKey } from "../../utils/color.util"
 function Home({ collections }) {
   return (
     <div className="max-w-7xl mx-auto">
-      <h1 className="font-bold mt-7 text-4xl text-gray-100">
+      <h1 className="font-bold mt-7 text-2xl sm:text-4xl text-gray-100 px-2">
         Converter collections
       </h1>
-      <div className="flex flex-wrap justify-evenly mt-10 border rounded-2xl border-gray-400 shadow p-8 gap-y-8">
+      <div className="flex flex-wrap justify-evenly mt-5 sm:mt-10 border rounded-2xl border-gray-400 shadow p-8 gap-y-8 m-2">
         {collections.map(({ short, label, path, description }, key) => (
-          <Link href={path}>
-            <a className="w-1/3 ">
-              <div className="flex bg-gray-900 hover:bg-gray-600 border rounded-xl border-gray-600 w-80">
+          <Link href={path} key={path}>
+            <a className="md:w-1/2 xl:w-1/3">
+              <div className="flex bg-gray-900 hover:bg-gray-600 border rounded-xl border-gray-600 w-60 sm:w-80">
                 <div>
                   <h1
                     className=" px-5 py-6 text-white bg rounded-xl  text-3xl"
@@ -23,10 +23,12 @@ function Home({ collections }) {
                   </h1>
                 </div>
                 <div>
-                  <h1 className="ml-3 mt-4 text-white font-bold text-lg ">
+                  <h1 className="ml-3 mt-6 sm:mt-4 text-white sm:font-bold text-2xl sm:text-lg">
                     {label}
                   </h1>
-                  <h1 className="text-gray-300 ml-3">{description}</h1>
+                  <h1 className="hidden sm:block text-gray-300 ml-3 break-words pr-2">
+                    {description}
+                  </h1>
                 </div>
               </div>
             </a>
@@ -34,7 +36,7 @@ function Home({ collections }) {
         ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default Home
