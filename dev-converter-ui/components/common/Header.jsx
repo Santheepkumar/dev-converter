03fc714323cd23fca4531e8b83887f25fc8f9d3a
Sanjaikumar <sanjaikumarbca@gmail.com>
2021-08-02 21:38:39 +0530

    noo changes

--- dev-converter-ui/components/common/Header.jsx
+++ dev-converter-ui/components/common/Header.jsx
@@ -1,5 +1,5 @@
-import Link from "next/link"
-import Image from "next/image"
+import Link from "next/link";
+import Image from "next/image";
 
 function Header() {
   return (
@@ -28,7 +28,7 @@ function Header() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
