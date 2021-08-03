a6a44bb670b2dc25a0f2bca2d947dac239043263
Sanjaikumar <sanjaikumarbca@gmail.com>
2021-08-03 23:49:17 +0530

    active converter  ui

---
+++ dev-converter-ui/components/common/ActiveConverter.jsx
@@ -0,0 +1,9 @@
+function Active() {
+  return (
+    <div className="flex w-56  text-white bg--400">
+      <h1 className="px-3 py-3">s</h1>
+      <h1>Active</h1>
+    </div>
+  );
+}
+export default Active;
