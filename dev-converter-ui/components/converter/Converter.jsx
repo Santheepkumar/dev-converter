24d8068659fc9ab7dc00911da51d7668a043ec69
Sanjaikumar <sanjaikumarbca@gmail.com>
2021-08-04 19:35:26 +0530

    Active converter imported

--- dev-converter-ui/components/converter/Converter.jsx
+++ dev-converter-ui/components/converter/Converter.jsx
@@ -1,6 +1,8 @@
 import ConverterOptions from "./ConverterOptions";
 import MainConvertion from "./MainConvertion";
 
+import { getColorByKey } from "../../utils/color.util";
+
 function Converter({ currConverter, setCC, converterFunction, converters }) {
   return (
     <div className="m-2 sm:m-8 mt-10 sm:mt-10">
@@ -9,6 +11,19 @@ function Converter({ currConverter, setCC, converterFunction, converters }) {
         setCC={setCC}
         currConverter={currConverter}
       />
+
+      <div className="text-white w-56 mt-5 flex border border-gray-500 rounded-md">
+        <h1
+          className="bg-pink-500 py-2 px-4 rounded-md text-3xl"
+          style={{
+            backgroundColor: getColorByKey(key)
+          }}
+        >
+          {currConverter.label.slice(0, 1)}
+        </h1>
+        <h1 className="ml-8 mt-3 text-xl">{currConverter.label}</h1>
+      </div>
+
       <MainConvertion
         converterFunction={converterFunction}
         currConverter={currConverter}
