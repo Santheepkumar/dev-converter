bb69b566f2768674e96173d47b54569395c48ff2
Santheep kumar <santheep.v@skematix.in>
2021-08-05 07:53:16 +0530

    login page ui alignment

--- dev-converter-ui/components/common/Login.jsx
+++ dev-converter-ui/components/common/Login.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function Login({ signInWithGoogle }) {
   return (
-    <div className="mt-10 text-center bg-white w-96 rounded-xl max-w-5xl mx-auto">
+    <div className="text-center bg-white w-96 rounded-xl mx-auto mt-48">
       <div className="py-20">
         <div className="flex justify-center ml-5">
           <Image
@@ -15,12 +15,15 @@ function Login({ signInWithGoogle }) {
         </div>
         <div className="mb-14">
           <h1 className="mt-5 text-3xl text-dark-800">Welcome</h1>
-          <h1 className="text-dark-600 mt-4">
-            Log the dev converter to convert your text
+          <h1 className="text-dark-600 mt-4 text-lg">
+            Log in to dev converter.
           </h1>
         </div>
-        <div className="mt-7 ml-7 w-80 b  ">
-          <h1 className="p-1 flex justify-center  text-dark-800 text-base  rounded-md border border-dark-300 bg-white ">
+        <div className="mt-7 ml-7 w-80">
+          <a
+            className="p-1 flex justify-center  text-dark-800 text-base  rounded-md border border-dark-300 bg-white cursor-pointer"
+            onClick={signInWithGoogle}
+          >
             <Image
               className="block h-5 "
               src={require("../assets/images/google.png")}
@@ -28,12 +31,10 @@ function Login({ signInWithGoogle }) {
               width="35"
               height="35"
             />
-            <button className="focus:outline-none" onClick={signInWithGoogle}>
-              Login with Google
-            </button>
-          </h1>
+            <div className="mt-1">Login with Google</div>
+          </a>
 
-          <h1 className="p-1 flex justify-center text-dark-800  mt-5 text-base border border-gray-300 rounded-md">
+          {/* <a className="p-1 flex justify-center text-dark-800  mt-5 text-base border border-gray-300 rounded-md">
             <Image
               className="block h-5"
               src={require("../assets/images/github.png")}
@@ -41,8 +42,8 @@ function Login({ signInWithGoogle }) {
               width="32"
               height="32"
             />
-            <button>Login with GitHub</button>
-          </h1>
+            <div className="mt-1">Login with GitHub</div>
+          </a> */}
         </div>
       </div>
     </div>
